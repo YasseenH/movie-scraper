2d3eda2f4c369b7f339469a85cad7ed106e6c87f
YasseenH <yasseenhilal@utexas.edu>
2024-03-14 00:55:45 -0500

    Fixed Movie

--- src/Movie.java
+++ src/Movie.java
@@ -1,24 +1,51 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Movie {
-	private final String TITLE;
-	private final int YEAR;
-	private final ArrayList<String> CAST;
-	private final String GENRE;
-	private final String DESCRIPTION;
-	private final double RATING;
-	private final String FILM_RATING;
-	private final String DURATION;
+	private String title;
+	private int year;
+	private ArrayList<String> cast;
+	private String genre;
+	private String description;
+	private double rating;
+	private String filmRating = "";
+	private String duration;
 	
-	public Movie(String title, int year, String filmRating, double fanRating, String duration, ArrayList<String> cast, String genre, String description) {
-		TITLE = title;
-		YEAR = year;
-		CAST = cast;
-		FILM_RATING = filmRating;
-		GENRE = genre;
-		DESCRIPTION = description;
-		RATING = fanRating;
-		DURATION = duration;
+	public Movie(String movieDetails) {
+		Scanner sc = new Scanner(movieDetails);
+		sc.next();
+		title = sc.next();
+		sc.nextLine();
+		sc.next();
+		year = sc.nextInt();
+		sc.nextLine();
+		sc.next();
+		rating = sc.nextDouble();
+		sc.next();
+		while (sc.hasNext()) {
+			filmRating += sc.next();
+		}
+		sc.nextLine();
+		while (sc.hasNext()) {
+			genre += sc.next();
+		}
+		sc.nextLine();
+		sc.next();
+		while (sc.hasNext()) {
+			description += sc.next();
+		}
+		sc.nextLine();
+		sc.useDelimiter(",");
+		while (sc.hasNext()) {
+			cast.add(sc.next());
+		}
+		sc.nextLine();
+		sc.reset();
+		sc.next();
+		while (sc.hasNext()) {
+			duration += sc.next();
+		}
+		sc.close();
 	}
 
 
@@ -26,14 +53,14 @@ public class Movie {
 	 * @return the rating
 	 */
 	public double getRating() {
-		return RATING;
+		return rating;
 	}
 	
 	/**
 	 * @return the duration
 	 */
 	public String getDuration() {
-		return DURATION;
+		return duration;
 	}
 
 
@@ -41,7 +68,7 @@ public class Movie {
 	 * @return the title
 	 */
 	public String getTitle() {
-		return TITLE;
+		return title;
 	}
 
 
@@ -49,7 +76,7 @@ public class Movie {
 	 * @return the year
 	 */
 	public int getReleaseYear() {
-		return YEAR;
+		return year;
 	}
 
 
@@ -57,23 +84,23 @@ public class Movie {
 	 * @return the description
 	 */
 	public String getDescription() {
-		return DESCRIPTION;
+		return description;
 	}
 	
 	public String getCastList() {
 		StringBuilder result = new StringBuilder();
-		result.append(CAST.get(0));
-		for (int i = 1; i < CAST.size(); i++) {
+		result.append(cast.get(0));
+		for (int i = 1; i < cast.size(); i++) {
 			result.append(",");
-			result.append(CAST.get(i));
+			result.append(cast.get(i));
 		}
 		return result.toString();
 	}
 	
 	public boolean isActorHere(String actor) {
 		String realName = actor.toLowerCase();
-		for (int i = 0; i < CAST.size(); i++) {
-			if (CAST.get(i).toLowerCase().equals(realName)) {
+		for (int i = 0; i < cast.size(); i++) {
+			if (cast.get(i).toLowerCase().equals(realName)) {
 				return true;
 			}
 		}
@@ -84,7 +111,7 @@ public class Movie {
 	 * @return a string representation of all the movie's details
 	 */
 	public String toString() {
-		return "Title: " + this.getTitle() + "\nYear: " + this.getReleaseYear() + "\nRating: " + this.getRating() + "\nFilm Rating: " + FILM_RATING
-		+ "\nGenre: " + GENRE + "\nDescription: " + DESCRIPTION + "\nCast: " + getCastList() + "\nDuration:" + DURATION + "\n";
+		return "Title: " + this.getTitle() + "\nYear: " + this.getReleaseYear() + "\nRating: " + this.getRating() + "\nFilm Rating: " + filmRating
+		+ "\nGenre: " + genre + "\nDescription: " + description + "\nCast: " + getCastList() + "\nDuration:" + duration + "\n";
 	}
 }
--- src/MovieManager.java
+++ src/MovieManager.java
@@ -1,4 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MovieManager {
 
+    private ArrayList<Movie> movies;
+
+    public MovieManager() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("ScrapedData.txt"));
+        while (sc.hasNextLine()) {
+            String tempEnd = sc.nextLine();
+            String allDetails = tempEnd;
+            while (!tempEnd.equals("")) {
+                tempEnd = sc.nextLine();
+                allDetails += "\n" + tempEnd;
+            }
+            System.out.println(allDetails);
+            Movie tempMovie = new Movie(allDetails);
+            movies.add(tempMovie);
+            System.out.println(allDetails);
+        }
+        sc.close();
+    }
+
+    
 }
